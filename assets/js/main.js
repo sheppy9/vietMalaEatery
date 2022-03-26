c6c48dc381e2c9e5d4773a708434518335faec3f
Randy <randyharrogates@gmail.com>
2022-03-26 18:52:02 +0800

    fixed #1 Done header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,84 +1,86 @@
+/** @format */
+
 // configurations
 let metadata = [
-    { name: 'shopName', content: 'Tê Tê' },
-    { name: '<metaName>', content: '<metaContent>' },
+	{ name: "shopName", content: "Tê Tê" },
+	{ name: "<metaName>", content: "<metaContent>" },
 ];
 
 let cards = [
-    {
-        activated: true,
-        type: 'header',
-        classes: 'bg-primary bg-gradient',
-    },
-    {
-        activated: true,
-        type: 'introduction',
-        classes: 'bg-secondary bg-gradient',
-    },
-    {
-        activated: true,
-        type: 'overview',
-        classes: 'bg-success bg-gradient',
-    },
-    {
-        activated: true,
-        type: 'updates',
-        classes: 'bg-danger bg-gradient',
-    },
-    {
-        activated: true,
-        type: 'menu',
-        classes: 'bg-warning bg-gradient',
-    },
-    {
-        activated: true,
-        type: 'reviews',
-        classes: 'bg-info bg-gradient',
-    },
-    {
-        activated: true,
-        type: 'gallery',
-        classes: 'bg-light bg-gradient',
-    },
-    {
-        activated: true,
-        type: 'map',
-        classes: 'bg-dark bg-gradient',
-    },
-    {
-        activated: true,
-        type: 'contact',
-        classes: 'bg-primary bg-gradient',
-    },
-    {
-        activated: true,
-        type: 'footer',
-        classes: 'bg-primary bg-gradient',
-    },
+	{
+		activated: true,
+		type: "header",
+		classes: "bg-light bg-gradient",
+		img: "assets/img/header/TeTe_logo.png",
+		text: "Tê Tê - Vịt lắc Mala",
+	},
+	{
+		activated: true,
+		type: "introduction",
+		classes: "bg-secondary bg-gradient",
+	},
+	{
+		activated: true,
+		type: "overview",
+		classes: "bg-success bg-gradient",
+	},
+	{
+		activated: true,
+		type: "updates",
+		classes: "bg-danger bg-gradient",
+	},
+	{
+		activated: true,
+		type: "menu",
+		classes: "bg-warning bg-gradient",
+	},
+	{
+		activated: true,
+		type: "reviews",
+		classes: "bg-info bg-gradient",
+	},
+	{
+		activated: true,
+		type: "gallery",
+		classes: "bg-light bg-gradient",
+	},
+	{
+		activated: true,
+		type: "map",
+		classes: "bg-dark bg-gradient",
+	},
+	{
+		activated: true,
+		type: "contact",
+		classes: "bg-primary bg-gradient",
+	},
+	{
+		activated: true,
+		type: "footer",
+		classes: "bg-primary bg-gradient",
+	},
 ];
 // END configurations
 
 // On page loaded
 $(function () {
+	// initialize the meta tags
+	metadata.forEach((meta) => {
+		$("head").append(`<meta name="${meta.name}" content="${meta.content}">`);
+	});
+	// END initialize the meta tags
 
-    // initialize the meta tags
-    metadata.forEach(meta => {
-        $("head").append(`<meta name="${meta.name}" content="${meta.content}">`);
-    });
-    // END initialize the meta tags
-
-    // creating Vue instance
-    Vue.createApp({
-        data () {
-            return {
-                cards: cards
-            };
-        },
-        mounted () {
-            let self = this;
-        }
-    }).mount("#main");
-    // END creating Vue instance
-
+	// creating Vue instance
+	Vue.createApp({
+		data() {
+			return {
+				cards: cards,
+			};
+		},
+		mounted() {
+			let self = this;
+		},
+	}).mount("#main");
+	// END creating Vue instance
 });
 // END On page loaded
