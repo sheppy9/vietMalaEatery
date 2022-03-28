ca9db0d2bb86ae724b6fc4c511b8b1fd45cd4983
kaiying <kaiying1991@hotmail.com>
2022-03-28 22:03:24 +0800

    added multiple language support sample

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,28 @@
 // configurations
-let metadata = [
+const metadata = [
     { name: 'shopName', content: 'Tê Tê' },
     { name: '<metaName>', content: '<metaContent>' },
 ];
 
-let cards = [
+const languages = [{ code: 'en', name: 'English' }, { code: 'vi', name: 'Tiếng Việt' }];
+const defaultLanguage = languages[0].code;
+const fallbackLanguage = languages[1].code;
+const i18nMessages = {
+    en: {
+        helloWorld: 'Hello World'
+    },
+    vi: {
+        helloWorld: 'Chào thế giới'
+    }
+};
+
+const cards = [
+    {
+        activated: true,
+        type: 'language',
+        classes: 'bg-primary bg-gradient',
+        languages: languages
+    },
     {
         activated: true,
         type: 'header',
@@ -62,22 +80,37 @@ let cards = [
 $(function () {
 
     // initialize the meta tags
-    metadata.forEach(meta => {
-        $("head").append(`<meta name="${meta.name}" content="${meta.content}">`);
-    });
+    metadata.forEach(meta => $("head").append(`<meta name="${meta.name}" content="${meta.content}">`));
     // END initialize the meta tags
 
+    // Creating i18n
+    const i18n = VueI18n.createI18n({
+        locale: defaultLanguage,
+        fallbackLocale: fallbackLanguage,
+        messages: i18nMessages,
+    });
+    // END Creating i18n
+
     // creating Vue instance
-    Vue.createApp({
+    var main = Vue.createApp({
         data () {
             return {
+                locale: languages[0].code,
                 cards: cards
             };
         },
+        methods: {
+            changeLocale () {
+                i18n.global.locale = this.locale;
+            }
+        },
         mounted () {
-            let self = this;
+            this.locale = languages.filter(language => language.code === defaultLanguage)[0].code;
         }
-    }).mount("#main");
+    });
+
+    main.use(i18n);
+    main.mount("#main");
     // END creating Vue instance
 
 });
